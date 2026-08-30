0cc34d3025b36b4087e0d85d316f8411797f6a30
dongrs <43883817+qjyd@users.noreply.github.com>
2026-08-30 19:59:35 +0800

    deploy: flutter_bootstrap.js

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"587c18f873b8ab57330422bce09047420d9c7f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "504737316"
+    serviceWorkerVersion: "3162452732"
   }
 });
